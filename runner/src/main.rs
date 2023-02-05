28ca99f933670f36ffec93f1412ef5ce10a1279f
shiomiyan <35842766+shiomiyan@users.noreply.github.com>
2023-02-05 19:46:19 +0900

    共通の型定義を生やした

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let hoge_s = String::from_utf8(hoge.to_owned()).unwrap();
     println!("{:?}", hoge_s);
 
-    let fuga_lib = plugin_loader::call_hoge_dynamic().unwrap();
+    let fuga_lib = plugin_loader::call_fuga_dynamic().unwrap();
     let fuga = unsafe {
         std::slice::from_raw_parts(fuga_lib.ptr, fuga_lib.len)
     };
